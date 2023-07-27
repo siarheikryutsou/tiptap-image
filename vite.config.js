2bac1f4a0ad46d771c76ce6ce5db7009621f9782
Sergey Krivtsov <flashgangsta@gmail.com>
2023-07-27 16:16:48 +0200

    changed base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tiptap-image",
+  base: "/tiptap-image/",
   plugins: [
     vue(),
   ],
