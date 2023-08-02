b5cab0bbab85df6bd2777b85cf60092fcc67f687
Sergey Krivtsov <flashgangsta@gmail.com>
2023-08-02 02:14:13 +0200

    Check images; Resize images; Focus border fix;

--- src/components/tip-tap-editor/TipTapEditor.vue
+++ src/components/tip-tap-editor/TipTapEditor.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="editor">
     <editor-content :editor="editor"/>
-    <input type="file" ref="elInputFile" style="display: none" @change="onFileChange"
-           :accept="ALLOWED_TYPES.toString()"/>
+    <input type="file" ref="elInputFile" style="display: none" @change="onFileSelect"
+           :accept="allowedTypes.toString()"/>
     <button @click="openFileInput">Add img</button>
   </div>
 </template>
@@ -19,15 +19,8 @@ const {} = defineComponent({
   }
 });
 
-const editor: Editor = new Editor({
-  content: '<p>Hello world</p>',
-  extensions: [
-    StarterKit,
-    Image
-  ],
-});
 
-const ALLOWED_TYPES: string[] = [
+const allowedTypes: string[] = [
   "image/bmp",
   "image/gif",
   "image/jpeg",
@@ -37,17 +30,96 @@ const ALLOWED_TYPES: string[] = [
   "image/webp",
   "image/x-icon",
 ];
-
+const MAX_FILE_SIZE_IN_MB:number = 15;
+const MAX_IMG_WIDTH:number = 2500;
+const MAX_IMG_HEIGHT:number = 2500;
 const elInputFile = ref<HTMLInputElement>();
 
 const openFileInput = (): void => {
   elInputFile.value?.click();
 }
 
-const onFileChange = (): void => {
+const onFileSelect = (event:Event): void => {
+  const input:HTMLInputElement = event.target as HTMLInputElement;
+  if(input && input.files) {
+    const [file] = input.files;
+    if (file) {
+      checkFile(file).then((url:string) => {
+        appendImage(url);
+      });
+    }
+  }
+}
+
+const onPaste = ():void => {
 
 }
 
+const checkFile = async (file:File):Promise<string> => {
+  return await new Promise((resolve, reject) => {
+    const fileSizeInMb = file.size / 1024 / 1024;
+    if(fileSizeInMb > MAX_FILE_SIZE_IN_MB) {
+      showFileSizeAlert(fileSizeInMb);
+      reject();
+    }
+    
+    if(!allowedTypes.includes(file.type)) {
+      showFileWrongTypeAlert(file.type);
+      reject();
+    }
+
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    img.onload = () => {
+      if(img.width > MAX_IMG_WIDTH || img.height > MAX_IMG_HEIGHT) {
+        const url = resizeImage(img);
+        resolve(url);
+      } else {
+        resolve(img.src);
+      }
+    }
+  })
+}
+
+
+const appendImage = (url:string):void => {
+  editor.chain().focus().setImage({src: url}).run();
+}
+
+
+const resizeImage = (img:HTMLImageElement):string => {
+  const canvas:HTMLCanvasElement | undefined = document.createElement("canvas");
+  const ctx:CanvasRenderingContext2D | null = canvas.getContext("2d");
+  const scaleRatio:number = Math.min(MAX_IMG_WIDTH / img.width, MAX_IMG_HEIGHT / img.height);
+  const newWidth:number = Math.floor(img.width * scaleRatio);
+  const newHeight:number = Math.floor(img.height * scaleRatio);
+  canvas.width = newWidth;
+  canvas.height = newHeight;
+  ctx?.drawImage(img, 0, 0, newWidth, newHeight);
+  return canvas.toDataURL();
+}
+
+
+const showFileSizeAlert = (size:number):void => {
+  alert(`Image need to be less than ${MAX_FILE_SIZE_IN_MB}mb in size. Your file is ${size.toFixed(2)}Mb`);
+}
+
+
+const showFileWrongTypeAlert = (type:string):void => {
+  alert(`Image need to be${allowedTypes.toString().split("image/").join(" ")}. Yur image is ${type.split("/")[1]}`);
+}
+
+
+const editor: Editor = new Editor({
+  content: '<p>Hello world</p>',
+  extensions: [
+    StarterKit,
+    Image
+  ],
+  editorProps: {
+    handlePaste: onPaste
+  }
+});
 
 </script>
 
@@ -68,7 +140,8 @@ const onFileChange = (): void => {
 }
 
 #editor :deep(img.ProseMirror-selectednode) {
-  border: 1px solid red;
+  outline: solid 2px red;
+  outline-offset: -2px;
 }
 
 </style>
--- src/components/tip-tap-editor/consts/ImgConsts.ts
+++
@@ -1,3 +0,0 @@
-export const MAX_SIZE_MB:number = 15;
-export const MAX_WIDTH:number = 2500;
-export const MAX_HEIGHT = 2500;
