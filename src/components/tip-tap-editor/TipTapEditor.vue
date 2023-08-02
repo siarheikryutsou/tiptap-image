0d088e4bfb466b85fff55e88268f3dfbc3508e84
Sergey Krivtsov <flashgangsta@gmail.com>
2023-08-02 02:51:42 +0200

    Added styles for focused image

--- src/components/tip-tap-editor/TipTapEditor.vue
+++ src/components/tip-tap-editor/TipTapEditor.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="editor">
     <editor-content :editor="editor"/>
-    <input type="file" ref="elInputFile" style="display: none" @change="onFileSelect"
-           :accept="allowedTypes.toString()"/>
-    <button @click="openFileInput">Add img</button>
+    <div class="controls">
+      <input type="file" ref="elInputFile" style="display: none" @change="onFileSelect"
+             :accept="allowedTypes.toString()"/>
+      <button @click="onAddImageButtonClick">Add img</button>
+      <button @click="onSaveClick">Save</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {EditorContent, Editor} from '@tiptap/vue-3'
-import StarterKit from '@tiptap/starter-kit'
-import Image from '@tiptap/extension-image'
+import {EditorContent, Editor} from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+import Image from '@tiptap/extension-image';
+import Focus from '@tiptap/extension-focus';
 import {defineComponent, ref} from "vue";
 
 const {} = defineComponent({
@@ -30,40 +34,47 @@ const allowedTypes: string[] = [
   "image/webp",
   "image/x-icon",
 ];
-const MAX_FILE_SIZE_IN_MB:number = 15;
-const MAX_IMG_WIDTH:number = 2500;
-const MAX_IMG_HEIGHT:number = 2500;
+const MAX_FILE_SIZE_IN_MB: number = 15;
+const MAX_IMG_WIDTH: number = 2500;
+const MAX_IMG_HEIGHT: number = 2500;
 const elInputFile = ref<HTMLInputElement>();
 
-const openFileInput = (): void => {
+const onAddImageButtonClick = (): void => {
   elInputFile.value?.click();
 }
 
-const onFileSelect = (event:Event): void => {
-  const input:HTMLInputElement = event.target as HTMLInputElement;
-  if(input && input.files) {
+const onFileSelect = (event: Event): void => {
+  const input: HTMLInputElement = event.target as HTMLInputElement;
+  if (input && input.files) {
     const [file] = input.files;
     if (file) {
-      checkFile(file).then((url:string) => {
+      checkFile(file).then((url: string) => {
         appendImage(url);
       });
     }
   }
 }
 
-const onPaste = ():void => {
-
+const onPaste = (): boolean => {
+  console.log("onPaste");
+  return false;
 }
 
-const checkFile = async (file:File):Promise<string> => {
+const onDrop = (): boolean => {
+  console.log("onDrop");
+  return false;
+}
+
+
+const checkFile = async (file: File): Promise<string> => {
   return await new Promise((resolve, reject) => {
     const fileSizeInMb = file.size / 1024 / 1024;
-    if(fileSizeInMb > MAX_FILE_SIZE_IN_MB) {
+    if (fileSizeInMb > MAX_FILE_SIZE_IN_MB) {
       showFileSizeAlert(fileSizeInMb);
       reject();
     }
-    
-    if(!allowedTypes.includes(file.type)) {
+
+    if (!allowedTypes.includes(file.type)) {
       showFileWrongTypeAlert(file.type);
       reject();
     }
@@ -71,7 +82,7 @@ const checkFile = async (file:File):Promise<string> => {
     const img = document.createElement("img");
     img.src = URL.createObjectURL(file);
     img.onload = () => {
-      if(img.width > MAX_IMG_WIDTH || img.height > MAX_IMG_HEIGHT) {
+      if (img.width > MAX_IMG_WIDTH || img.height > MAX_IMG_HEIGHT) {
         const url = resizeImage(img);
         resolve(url);
       } else {
@@ -82,17 +93,17 @@ const checkFile = async (file:File):Promise<string> => {
 }
 
 
-const appendImage = (url:string):void => {
+const appendImage = (url: string): void => {
   editor.chain().focus().setImage({src: url}).run();
 }
 
 
-const resizeImage = (img:HTMLImageElement):string => {
-  const canvas:HTMLCanvasElement | undefined = document.createElement("canvas");
-  const ctx:CanvasRenderingContext2D | null = canvas.getContext("2d");
-  const scaleRatio:number = Math.min(MAX_IMG_WIDTH / img.width, MAX_IMG_HEIGHT / img.height);
-  const newWidth:number = Math.floor(img.width * scaleRatio);
-  const newHeight:number = Math.floor(img.height * scaleRatio);
+const resizeImage = (img: HTMLImageElement): string => {
+  const canvas: HTMLCanvasElement = document.createElement("canvas");
+  const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+  const scaleRatio: number = Math.min(MAX_IMG_WIDTH / img.width, MAX_IMG_HEIGHT / img.height);
+  const newWidth: number = Math.floor(img.width * scaleRatio);
+  const newHeight: number = Math.floor(img.height * scaleRatio);
   canvas.width = newWidth;
   canvas.height = newHeight;
   ctx?.drawImage(img, 0, 0, newWidth, newHeight);
@@ -100,26 +111,46 @@ const resizeImage = (img:HTMLImageElement):string => {
 }
 
 
-const showFileSizeAlert = (size:number):void => {
+const showFileSizeAlert = (size: number): void => {
   alert(`Image need to be less than ${MAX_FILE_SIZE_IN_MB}mb in size. Your file is ${size.toFixed(2)}Mb`);
 }
 
 
-const showFileWrongTypeAlert = (type:string):void => {
+const showFileWrongTypeAlert = (type: string): void => {
   alert(`Image need to be${allowedTypes.toString().split("image/").join(" ")}. Yur image is ${type.split("/")[1]}`);
 }
 
 
+const onSaveClick = ():void => {
+  save();
+}
+
+
+const save = ():void => {
+  console.log(editor.getJSON());
+}
+
+console.log("setup")
+
 const editor: Editor = new Editor({
   content: '<p>Hello world</p>',
   extensions: [
-    StarterKit,
-    Image
+      StarterKit,
+      Image.configure({
+        HTMLAttributes: {
+          class: "user-image"
+        }
+      }),
+      Focus.configure({
+        className: 'focus',
+      })
   ],
   editorProps: {
-    handlePaste: onPaste
+    handlePaste: onPaste,
+    handleDrop: onDrop
   }
 });
+
 
 </script>
 
@@ -135,13 +166,20 @@ const editor: Editor = new Editor({
   border: 1px solid black;
 }
 
-#editor :deep(img) {
+#editor :deep(.user-image) {
   max-width: 100%;
 }
 
-#editor :deep(img.ProseMirror-selectednode) {
+#editor :deep(.user-image.focus) {
   outline: solid 2px red;
   outline-offset: -2px;
+}
+
+#editor .controls {
+  margin-top: 10px;
+  & button {
+    margin-right: 5px;
+  }
 }
 
 </style>
